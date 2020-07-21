8df0f0d42848f9562dcbdd1310a3a70ae30f8ed4
Monica Bustamante <lilianalambda@gmail.com>
2020-07-21 15:16:01 -0700

    Recursive sorting solution

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,75 @@
 # TO-DO: Implement a recursive implementation of binary search
+import math
+
+
 def binary_search(arr, target, start, end):
-    # Your code here
+    if end - start + 1 <= 0:
+        return False
+    else:
+        midpoint = start + (end - start) // 2
+        if arr[midpoint] == target:
+            return midpoint
+        else:
+            if target < arr[midpoint]:
+                return binary_search(arr, target, start, midpoint -1)
+            else:
+                return binary_search(arr, target, midpoint + 1, end)
+
+
+def binary_search(arr, target, start, end):
+    if start >= end:
+        mid = (start + end) // 2
+        if arr[mid] > target:
+            return binary_search(arr, target, start, mid -1)
+        elif arr[mid] < target:
+            return binary_search(arr, target, mid + 1, start)
+        elif arr[mid] == target:
+            return mid
+    return -1
 
 
 # STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
+#This version of binary search should correctly find 
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
+# def agnostic_binary_search(arr, target):
+#     middle = (low + high) //2
 
+#     if len(arr) == 0:
+#         return -1 # array emptye
+
+
+def agnostic_binary_search(arr, target, min=0, max=None):
+    if not arr[0] <= target <= arr[-1]:
+        return False
+    if max is None:
+        max = len(arr) -1
+
+    mid - (min + max) // 2
+
+    if arr[mid] == target:
+        return mid 
+    elif target < arr[mid]:
+        max = mid - 1
+    elif arr[mid] < target:
+        min = mid + 1
+    return agnostic_binary_search(arr, target, min, max)
+
+def agnostic_binary_search(arr, target):
+    first = 0
+    last = len(arr)-1
+    found = False
+	
+    while first<=last and not found:
+          midpoint = (first + last)//2
+          if arr[midpoint] == target:
+                found = True
+          else:
+              if target < arr[midpoint]:
+                    last = midpoint-1
+              else:
+                    first = midpoint+1
+    
+    return found
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,28 +1,39 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
+import math
+
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
-
-    # Your code here
-
-
+    # elements = len(arrA) + len(arrB)
+    merged_arr = []
+    while(len(arrA) and len(arrB)):
+        if(arrA[0] < arrB[0]):
+            merged_arr.append(arrA.pop(0))
+        else:
+            merged_arr.append(arrB.pop(0))
+    while(len(arrA)):
+        merged_arr.append(arrA.pop(0))
+    while(len(arrB)):
+        merged_arr.append(arrB.pop(0))
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
+    if(len(arr) < 2):
+        return arr
+    middle = math.floor(len(arr)/2)
+    left = arr[:middle]
+    right = arr[middle:]
+    arrA = merge_sort(left)
+    arrB = merge_sort(right)
+    return merge(arrA, arrB)
+print(merge_sort([1,3,5,8,6,2,4,7,9]))
 
-
-    return arr
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
 
-
-def merge_sort_in_place(arr, l, r):
-    # Your code here
-
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
+# def merge_sort_in_pace(arr, l, r):
+#     # Your code here
